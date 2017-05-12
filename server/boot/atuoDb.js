90a0172f817afb61339d024513d73a473bc783c5
Beck <beck.zhang@softtek.com>
2017-05-12 11:33:17 +0800

    init commit

---
+++ server/boot/atuoDb.js
@@ -0,0 +1,14 @@
+/**
+ * Created by beck.zhang on 5/12/2017.
+ */
+module.exports = function (app) {
+
+ /* var server = require('../server');
+  var ds = app.dataSources.mysqlDB;
+  var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
+  ds.automigrate(lbTables, function(er) {
+    if (er) throw er;
+    console.log('Loopback tables [' - lbTables - '] created in ', ds.adapter.name);
+    ds.disconnect();
+  });*/
+};
