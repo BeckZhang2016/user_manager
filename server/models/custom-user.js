ed63d25203c4580e3ba92dfa3c3dc9fc9ba7a5fd
Beck <beck.zhang@softtek.com>
2017-05-12 15:48:25 +0800

    from beck

--- server/models/custom-user.js
+++ server/models/custom-user.js
@@ -1,5 +1,36 @@
 'use strict';
 
-module.exports = function(Customuser) {
-
+module.exports = function (Customuser) {
+  Customuser.userLogin = function (body, cb) {
+    Customuser.login(body, function (err, token) {
+      if (err) throw err;
+      cb(null, token);
+    });
+  };
+  Customuser.remoteMethod(
+    'userLogin',
+    {
+      http: {
+        path: '/userLogin',
+        verb: 'post'
+      },
+      accepts: {
+        arg: 'object',
+        type: 'object',
+        http: {
+          source: 'body'
+        }
+      },
+      returns: [
+        {
+          arg: 'email',
+          type: 'string'
+        },
+        {
+          arg: 'password',
+          type: 'string'
+        }
+      ]
+    }
+  )
 };
